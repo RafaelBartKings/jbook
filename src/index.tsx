b6477c75c1d93f58651aca4f03a800f1c0697dc1
Rafael  Reis <rafyangtj@gmail.com>
2022-11-15 06:10:23 -0300

    first jbook commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TextEditor from './components/text-editor';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+
+const App = () => {
+   return (
+      <div>
+         <TextEditor />
+      </div>
+   )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+)
